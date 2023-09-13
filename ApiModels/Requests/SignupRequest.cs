563ba0908a264558e9e945aedf433ab35a072409
Mateo <mateo.goldwasser@hotmail.com>
2023-09-13 14:24:46 -0300

    Signup_Test_OK green

---
+++ ApiModels/Requests/SignupRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiModels.Requests
+{
+    public class SignupRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
