3ac3e092da9871f638ba25f4c5ac086d52690b9d
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-09-19 15:44:52 -0300

    [GREEN] ProductLogic GetProduct InvalidId

--- Logic.Tests/ProductLogicTest.cs
+++ Logic.Tests/ProductLogicTest.cs
@@ -22,4 +22,21 @@ public class ProductLogicTest
         // Assert
         Assert.AreEqual(product, result);
     }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException), "Product with id 0 not found")]
+    public void GetProduct_InvalidId_FAIL()
+    {
+        // Arrange
+        var product = new Product { Name = "Test", Price = 420, Description = "Test Description"};
+        var mock = new Mock<IProductRepository>();
+        mock.Setup(x => x.GetProduct(product.Id)).Throws(new ArgumentException("Product with id 0 not found"));
+        var logic = new ProductLogic(mock.Object);
+        
+        // Act
+        var result = logic.GetProduct(product.Id);
+        
+        // Assert
+        // Exception
+    }
 }
