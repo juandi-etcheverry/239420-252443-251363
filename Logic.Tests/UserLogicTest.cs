9181e152d6bdf65a41c97004475560eb06ee5569
NicolasEdelman <NE251363@fi365.ort.edu.uy>
2023-09-21 20:35:06 -0300

    [Green] GetUser_Invalid_FAIL

--- Logic.Tests/UserLogicTest.cs
+++ Logic.Tests/UserLogicTest.cs
@@ -27,6 +27,25 @@ namespace Logic.Tests
 			//Assert
 			Assert.AreEqual(user, result);
 		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException), "User with id 0 not found")]
+		public void GetUser_Invalid_FAIL()
+		{
+            //Arrange
+            var user = new User
+            {
+                Email = "userTest@gmail.com",
+                Role = Role.Comprador,
+                Address = "Ejido 1234"
+            };
+            var mock = new Mock<IUserRepository>(MockBehavior.Strict);
+            mock.Setup(x => x.GetUser(user.Id)).Throws(new ArgumentException("User with id 0 not found"));
+			var logic = new UserLogic(mock.Object);
+
+			//Act
+			var result = logic.GetUser(user.Id);
+        }
 	}
 }
 
