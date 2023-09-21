30ccd64cb62eb1cb80be12674ec48d56eaf31da8
NicolasEdelman <NE251363@fi365.ort.edu.uy>
2023-09-21 18:10:37 -0300

    [Green] AddUser_AddUserTwice_Fail

--- DataAccess.Tests/UserRepositoryTest.cs
+++ DataAccess.Tests/UserRepositoryTest.cs
@@ -33,6 +33,29 @@ namespace DataAccess.Tests
             //Assert
             Assert.AreEqual(user, result);
         }
+
+        [TestMethod]
+        public void AddUser_AddUserTwice_Fail()
+        {
+            //Arrgange
+            var context = CreateDbContext("AddUser_CorrectUser_OK");
+            var userRepository = new UserRepository(context);
+
+            var user = new User
+            {
+                Email = "test@gmail.com",
+                Role = Role.Comprador,
+                Address = "Mercedes 2331"
+            };
+
+            //Act
+            userRepository.AddUser(user);
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => userRepository.AddUser(user));
+        }
+
+
     }
 }
 
