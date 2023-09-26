d754841a3ba5f47fb85e490d0fbcd24c17a2887e
Mateo <mateo.goldwasser@hotmail.com>
2023-09-26 08:39:10 -0300

    User cannot signup to be an admin

--- ApiModels/Requests/Users/SignupRequest.cs
+++ ApiModels/Requests/Users/SignupRequest.cs
@@ -12,6 +12,5 @@ namespace ApiModels.Requests.Users
         public string Email { get; set; }
         public string Password { get; set; }
         public string PasswordConfirmation { get; set; }
-        public Role Role { get; set; }
     }
 }
