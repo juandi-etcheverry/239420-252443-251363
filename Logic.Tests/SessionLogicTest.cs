90a428768667d85c3fd0692a21757610f17577a6
NicolasEdelman <NE251363@fi365.ort.edu.uy>
2023-09-28 15:42:21 -0300

    [Green] Logic-GetSession_InvalidId_FAIL

--- Logic.Tests/SessionLogicTest.cs
+++ Logic.Tests/SessionLogicTest.cs
@@ -32,4 +32,27 @@ public class SessionLogicTest
         //Assert
         Assert.AreEqual(session, result);
     }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException), "Session  not found")]
+    public void GetSession_InvalidId_FAIL()
+    {
+        //Arrange
+        var user = new User
+        {
+            Email = "test@gmail.com",
+            Role = Role.Comprador,
+            Address = "Cuareim 1234",
+        };
+        var session = new SessionToken
+        {
+            User = user
+        };
+        var mock = new Mock<ISessionRepository>(MockBehavior.Strict);
+        mock.Setup(x => x.GetSessionToken(session.Id)).Throws(new ArgumentException("Session  not found"));
+        var logic = new SessionTokenLogic(mock.Object);
+        
+        //Act
+        var result = logic.GetSessionToken(session.Id);
+    }
 }
