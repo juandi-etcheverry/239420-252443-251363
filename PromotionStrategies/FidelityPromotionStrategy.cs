988ed4e6fef1366c708b1c7eeb43f4bfe6634957
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 17:11:21 -0300

    [REFACTOR] FidelityPromotionStrategy

--- PromotionStrategies/FidelityPromotionStrategy.cs
+++ PromotionStrategies/FidelityPromotionStrategy.cs
@@ -10,11 +10,23 @@ public class FidelityPromotionStrategy : IPromotionStrategy
     {
         var validProducts = products.FindAll(p => !p.IsDeleted);
         if (validProducts.Count < 3) return 0;
-        var uniqueBrands = validProducts.Select(p => p.Brand).Distinct().ToList();
-        var brandsWithThreeProducts = uniqueBrands.FindAll(b => validProducts.FindAll(p => p.Brand == b).Count >= 3);
-        var filteredProducts = validProducts.FindAll(p => brandsWithThreeProducts.Contains(p.Brand));
-        var orderedProducts = filteredProducts.OrderBy(p => p.Price);
+        var orderedProducts = OrderedProductsValidForPromotion(validProducts);
         var discount = orderedProducts.TakeWhile((_, idx) => idx < 2).ToList().Sum(p => p.Price);
         return discount;
+    }
+
+    private List<Product> OrderedProductsValidForPromotion(List<Product> validProducts)
+    {
+        var brandsWithThreeProducts = BrandsWithThreeProducts(validProducts);
+        var filteredProducts = validProducts.FindAll(p => brandsWithThreeProducts.Contains(p.Brand));
+        var orderedProducts = filteredProducts.OrderBy(p => p.Price);
+        return orderedProducts.ToList();
+    }
+
+    private static List<Brand> BrandsWithThreeProducts(List<Product> validProducts)
+    {
+        var uniqueBrands = validProducts.Select(p => p.Brand).Distinct().ToList();
+        var brandsWithThreeProducts = uniqueBrands.FindAll(b => validProducts.FindAll(p => p.Brand == b).Count >= 3);
+        return brandsWithThreeProducts;
     }
 }
