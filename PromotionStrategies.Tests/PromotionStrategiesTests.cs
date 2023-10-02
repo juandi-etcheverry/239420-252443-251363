99dc461b115bf611227f9a387f33317331af7040
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 13:23:31 -0300

    [REFACTOR] TotalLookPromotionStrategy

--- PromotionStrategies.Tests/PromotionStrategiesTests.cs
+++ PromotionStrategies.Tests/PromotionStrategiesTests.cs
@@ -238,4 +238,171 @@ public class PromotionStrategiesTests
         //Assert
         Assert.AreEqual(150, result);
     }
+    
+    [TestMethod]
+    public void TotalLook_DifferentColorsAndDeleted_OK()
+    {
+        //Arrange
+        var strategy = new TotalLookPromotionStrategy();
+        var c1 = new Color() { Name = "Red" };
+        var c2 = new Color() { Name = "Blue" };
+        var c3 = new Color() { Name = "Green" };
+
+        var product1 = new Product()
+        {
+            Price = 100,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1, c2 },
+            Name = "Product 1",
+            IsDeleted = true
+        };
+
+        var product2 = new Product()
+        {
+            Price = 190,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1,c2 },
+            Name = "Product 2",
+            IsDeleted = true
+        };
+
+        var product3 = new Product()
+        {
+            Price = 300,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1 },
+            Name = "Product 3",
+            IsDeleted = true
+        };
+
+        var product4 = new Product()
+        {
+            Price = 420,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c3 },
+            IsDeleted = true
+        };
+        
+        var products = new List<Product>() { product1, product2, product3, product4 };
+        
+        //Act
+        var result = strategy.GetDiscount(products);
+        
+        //Assert
+        Assert.AreEqual(0, result);
+    }
+    
+    [TestMethod]
+    public void TotalLook_DifferentColorsAndDeletedAndNotDeleted_OK()
+    {
+        //Arrange
+        var strategy = new TotalLookPromotionStrategy();
+        var c1 = new Color() { Name = "Red" };
+        var c2 = new Color() { Name = "Blue" };
+        var c3 = new Color() { Name = "Green" };
+
+        var product1 = new Product()
+        {
+            Price = 100,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1, c2 },
+            Name = "Product 1",
+            IsDeleted = true
+        };
+
+        var product2 = new Product()
+        {
+            Price = 190,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1,c2 },
+            Name = "Product 2",
+            IsDeleted = true
+        };
+
+        var product3 = new Product()
+        {
+            Price = 300,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1 },
+            Name = "Product 3",
+            IsDeleted = false
+        };
+
+        var product4 = new Product()
+        {
+            Price = 420,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c3 },
+            IsDeleted = false
+        };
+        
+        var products = new List<Product>() { product1, product2, product3, product4 };
+        
+        //Act
+        var result = strategy.GetDiscount(products);
+        
+        //Assert
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void TotalLook_DifferentColorsLessThanThree_OK()
+    {
+        //Arrange
+        var strategy = new TotalLookPromotionStrategy();
+        var c1 = new Color() { Name = "Red" };
+        var c2 = new Color() { Name = "Blue" };
+        var c3 = new Color() { Name = "Green" };
+
+        var product1 = new Product()
+        {
+            Price = 100,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c2 },
+            Name = "Product 1",
+        };
+
+        var product2 = new Product()
+        {
+            Price = 190,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1, c2 },
+            Name = "Product 2",
+        };
+
+        var product3 = new Product()
+        {
+            Price = 300,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c1 },
+            Name = "Product 3",
+        };
+
+        var product4 = new Product()
+        {
+            Price = 420,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { c3 },
+        };
+
+        var products = new List<Product>() { product1, product2, product3, product4 };
+
+        //Act
+        var result = strategy.GetDiscount(products);
+
+        //Assert
+        Assert.AreEqual(0, result);
+    }
 }
--- PromotionStrategies/TotalLookPromotionStrategy.cs
+++ PromotionStrategies/TotalLookPromotionStrategy.cs
@@ -2,7 +2,11 @@
 using Logic.Interfaces;
 
 namespace PromotionStrategies;
-
+/*
+ POSSIBLE OPTIMIZATIONS :
+    - Use a dictionary to store the colors and a tuple with the amount
+    of products and the max price.
+ */
 public class TotalLookPromotionStrategy : IPromotionStrategy
 {
     private const float DiscountPercentage = 0.5f;
@@ -19,14 +23,26 @@ public class TotalLookPromotionStrategy : IPromotionStrategy
         return products.FindAll(p => !p.IsDeleted);
     }
 
+    // Due to how distinct works, the products MUST share the same reference to the colors
+    // otherwise, the distinct will not work as expected
     private float GetFinalDiscountAmount(List<Product> products)
     {
-        var uniqueColors = products.SelectMany(p => p.Colors).Distinct().ToList();
-        var colorsWithAtLeastThreeProducts =
-            uniqueColors.FindAll(c => products.FindAll(p => p.Colors.Contains(c)).Count >= 3);
-        if (colorsWithAtLeastThreeProducts.Count == 0) return 0;
-        var validProducts = products.FindAll(p => p.Colors.Any(c => colorsWithAtLeastThreeProducts.Contains(c)));
+        var colors = GetUniqueColorsWithAtLeastThreeProducts(products);
+        if (colors.Count == 0) return 0;
+        var validProducts = GetValidProducts(products, colors);
         return validProducts.Max(p => p.Price) * DiscountPercentage;
+    }
+
+    private List<Color> GetUniqueColorsWithAtLeastThreeProducts(List<Product> products)
+    {
+        var uniqueColors = products.SelectMany(p => p.Colors).Distinct().ToList();
+        return uniqueColors.FindAll(c => products.FindAll(p => p.Colors.Contains(c)).Count >= 3);
+    }
+    
+    private List<Product> GetValidProducts(List<Product> products, List<Color> colors)
+    {
+        var validProducts = products.FindAll(p => p.Colors.Any(c => colors.Contains(c)));
+        return validProducts;
     }
 
 }
