b19210b9659cd82715df28050debf1dae2e35054
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 02:29:12 -0300

    [GREEN] TwentyPercent DeletedAndNotDeleted OK

--- PromotionStrategies.Tests/PromotionStrategiesTests.cs
+++ PromotionStrategies.Tests/PromotionStrategiesTests.cs
@@ -78,4 +78,58 @@ public class PromotionStrategiesTests
         //Assert
         Assert.AreEqual(0, result);
     }
+
+    [TestMethod]
+    public void TwentyPercent_DeletedAndNotDeleted_OK()
+    {
+        //Arrange
+        var strategy = new TwentyPercentPromotionStrategy();
+        var product1 = new Product()
+        {
+            Price = 100,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 1",
+            IsDeleted = true
+        };
+
+        var product2 = new Product()
+        {
+            Price = 190,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 2",
+            IsDeleted = true
+        };
+
+        var product3 = new Product()
+        {
+            Price = 300,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "    Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 3",
+            IsDeleted = false
+        };
+        
+        var product4 = new Product()
+        {
+            Price = 400,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 4",
+            IsDeleted = false
+        };
+        
+        var products = new List<Product>() { product1, product2, product3, product4 };
+
+        //Act
+        var result = strategy.GetDiscount(products);
+
+        //Assert
+        Assert.AreEqual(80, result);
+    }
 }
