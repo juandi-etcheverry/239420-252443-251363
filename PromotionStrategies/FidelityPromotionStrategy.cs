f889ecbbfb09d2b89aa024414edd600b9bc6bf30
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 17:08:48 -0300

    [GREEN] Fidelity DifferentBrandsDeleted OK

--- PromotionStrategies/FidelityPromotionStrategy.cs
+++ PromotionStrategies/FidelityPromotionStrategy.cs
@@ -8,10 +8,11 @@ public class FidelityPromotionStrategy : IPromotionStrategy
     public string Name => "Fidelity Promotion";
     public float GetDiscount(List<Product> products)
     {
-        if (products.Count < 3) return 0;
-        var uniqueBrands = products.Select(p => p.Brand).Distinct().ToList();
-        var brandsWithThreeProducts = uniqueBrands.FindAll(b => products.FindAll(p => p.Brand == b).Count >= 3);
-        var filteredProducts = products.FindAll(p => brandsWithThreeProducts.Contains(p.Brand));
+        var validProducts = products.FindAll(p => !p.IsDeleted);
+        if (validProducts.Count < 3) return 0;
+        var uniqueBrands = validProducts.Select(p => p.Brand).Distinct().ToList();
+        var brandsWithThreeProducts = uniqueBrands.FindAll(b => validProducts.FindAll(p => p.Brand == b).Count >= 3);
+        var filteredProducts = validProducts.FindAll(p => brandsWithThreeProducts.Contains(p.Brand));
         var orderedProducts = filteredProducts.OrderBy(p => p.Price);
         var discount = orderedProducts.TakeWhile((_, idx) => idx < 2).ToList().Sum(p => p.Price);
         return discount;
