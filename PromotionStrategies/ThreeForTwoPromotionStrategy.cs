d7cd7d689baff9b7590398a0bec1be065ecfb7d8
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 14:23:35 -0300

    [REFACTOR] ThreeForTwoPromotionStrategy

--- PromotionStrategies/ThreeForTwoPromotionStrategy.cs
+++ PromotionStrategies/ThreeForTwoPromotionStrategy.cs
@@ -10,10 +10,22 @@ public class ThreeForTwoPromotionStrategy : IPromotionStrategy
     {
         var filteredProducts = products.FindAll(p => !p.IsDeleted);
         if (filteredProducts.Count < 3) return 0f;
-        var uniqueCategories = filteredProducts.Select(p => p.Category).Distinct().ToList();
-        var categoriesWithAtLeastThreeProducts = uniqueCategories.FindAll(c => filteredProducts.FindAll(p => p.Category == c).Count >= 3);
-        if (categoriesWithAtLeastThreeProducts.Count == 0) return 0f;
-        var validProducts = filteredProducts.FindAll(p => categoriesWithAtLeastThreeProducts.Contains(p.Category));
+        var categories = CategoriesWithAtLeastThreeProducts(filteredProducts);
+        if (categories.Count == 0) return 0f;
+        return GetDiscountAmount(filteredProducts, categories);
+    }
+
+    private float GetDiscountAmount(List<Product> filteredProducts, List<Category> categories)
+    {
+        var validProducts = filteredProducts.FindAll(p => categories.Contains(p.Category));
         return validProducts.Min(p => p.Price) * DiscountPercentage;
+    }
+
+    private List<Category> CategoriesWithAtLeastThreeProducts(List<Product> filteredProducts)
+    {
+        var uniqueCategories = filteredProducts.Select(p => p.Category).Distinct().ToList();
+        var categoriesWithAtLeastThreeProducts =
+            uniqueCategories.FindAll(c => filteredProducts.FindAll(p => p.Category == c).Count >= 3);
+        return categoriesWithAtLeastThreeProducts;
     }
 }
