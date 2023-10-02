1bf8a403f08b5658f76195af1bc7418309ef1818
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-01 23:56:01 -0300

    [GREEN] TwentyPercent TwoDeletedProducts OK

--- PromotionStrategies.Tests/PromotionStrategiesTests.cs
+++ PromotionStrategies.Tests/PromotionStrategiesTests.cs
@@ -44,4 +44,38 @@ public class PromotionStrategiesTests
         //Assert
         Assert.AreEqual(38, result);
     }
+
+    [TestMethod]
+    public void TwentyPercent_TwoDeletedProducts_OK()
+    {
+        //Arrange
+        var strategy = new TwentyPercentPromotionStrategy();
+        var product1 = new Product()
+        {
+            Price = 100,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 1",
+            IsDeleted = true
+        };
+
+        var product2 = new Product()
+        {
+            Price = 190,
+            Brand = new Brand() { Name = "Brand" },
+            Category = new Category() { Name = "Category" },
+            Colors = new List<Color> { new() { Name = "Red" }, new() { Name = "Blue" } },
+            Name = "Product 2",
+            IsDeleted = true
+        };
+
+        var products = new List<Product>() { product1, product2 };
+
+        //Act
+        var result = strategy.GetDiscount(products);
+
+        //Assert
+        Assert.AreEqual(0, result);
+    }
 }
--- PromotionStrategies/TwentyPercentPromotionStrategy.cs
+++ PromotionStrategies/TwentyPercentPromotionStrategy.cs
@@ -7,8 +7,9 @@ public class TwentyPercentPromotionStrategy : IPromotionStrategy
 {
     public float GetDiscount(List<Product> products)
     {
-        if (products.Count < 2) return 0;
+        var filteredProducts = products.FindAll(p => !p.IsDeleted);
+        if (filteredProducts.Count < 2) return 0;
         var discountPercentage = 20f / 100f;
-        return products.Max(x => x.Price) * discountPercentage;
+        return filteredProducts.Max(p => p.Price) * discountPercentage;
     }
 }
