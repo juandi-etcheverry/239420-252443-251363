814c5db818f0cacf8a902bb8f0f54c7efeea9afa
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-10-02 02:33:28 -0300

    [REFACTOR] TwentyPercentPromotionStrategy

--- PromotionStrategies/TwentyPercentPromotionStrategy.cs
+++ PromotionStrategies/TwentyPercentPromotionStrategy.cs
@@ -5,11 +5,22 @@ namespace PromotionStrategies;
 
 public class TwentyPercentPromotionStrategy : IPromotionStrategy
 {
+    private const float DiscountPercentage = 0.2f;    
+    
     public float GetDiscount(List<Product> products)
     {
-        var filteredProducts = products.FindAll(p => !p.IsDeleted);
+        var filteredProducts = FilterValidProductsForPromotion(products);
         if (filteredProducts.Count < 2) return 0;
-        var discountPercentage = 20f / 100f;
-        return filteredProducts.Max(p => p.Price) * discountPercentage;
+        return GetFinalDiscountAmount(filteredProducts);
+    }
+    
+    private List<Product> FilterValidProductsForPromotion(List<Product> products)
+    {
+        return products.FindAll(p => !p.IsDeleted);
+    }
+    
+    private float GetFinalDiscountAmount(List<Product> products)
+    {
+        return products.Max(p => p.Price) * DiscountPercentage;
     }
 }
