9da8edba2bde59289358f25db15e858960d20129
NicolasEdelman <NE251363@fi365.ort.edu.uy>
2023-10-24 20:59:49 -0300

    ProductDetailPage

--- FrontEndProject/src/app/app-routing.module.ts
+++ FrontEndProject/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { ProductsComponent } from './products/products.component';
 import { UserComponent } from './user/user.component';
 import { CartComponent } from './cart/cart.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'products', pathMatch: 'full'},
   {path: 'products', component: ProductsComponent},
+  {path: 'products/:id', component: ProductDetailComponent},
   {path: 'user', component: UserComponent},
   {path: 'cart', component: CartComponent},
   {path: '**', component: PagenotfoundComponent}
--- FrontEndProject/src/app/products/product-card/product-card.component.ts
+++ FrontEndProject/src/app/products/product-card/product-card.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule} from '@angular/material/card'; 
 import { ProductItem } from '../product-item';
-import {MatRippleModule} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatRippleModule} from '@angular/material/core';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule} from '@angular/forms';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatCheckboxModule, FormsModule, MatFormFieldModule, MatInputModule, MatRippleModule],
+  imports: [MatCardModule, MatButtonModule, MatCheckboxModule, FormsModule, MatFormFieldModule, MatInputModule, MatRippleModule, RouterModule],
 })
 export class ProductCardComponent implements OnInit{
   @Input()
@@ -21,17 +22,19 @@ export class ProductCardComponent implements OnInit{
   centered = false;
   disabled = false;
   unbounded = false;
-  constructor(){
+  constructor(private router:Router){
   }
 
   ngOnInit(): void {
   }
 
-  onAddClick($event : any){
-    alert("agregar al carrito");
+  onAddClick($event : Event){
+    $event.stopPropagation()
+    alert("Agregar al carrito");
   }
 
-  onCardClick(){
-    alert("ir a ver el producto");
+  onCardClick(id: number){
+    const url = 'products/' + id;
+    this.router.navigate([url]);
   }
 }
