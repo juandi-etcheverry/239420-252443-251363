e50ecf002e370441eb8203ecdc6f1b63d1a824e3
Mateo <mateo.goldwasser@hotmail.com>
2023-10-30 13:22:17 -0300

    updated getproducts request and response

--- ApiModels/Responses/Products/GetProductsResponse.cs
+++ ApiModels/Responses/Products/GetProductsResponse.cs
@@ -11,22 +11,24 @@ public class GetProductsResponse
     {
         return new ProductReponseObject
         {
+            Id = product.Id,
             Name = product.Name,
             Description = product.Description,
             Price = product.Price,
-            Category = product.Category,
-            Brand = product.Brand,
-            Colors = product.Colors
+            Category = product.Category.Name,
+            Brand = product.Brand.Name,
+            Colors = product.Colors.Select(c => c.Name).ToList()
         };
     }
 }
 
 public class ProductReponseObject
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public float Price { get; set; }
-    public Category Category { get; set; }
-    public Brand Brand { get; set; }
-    public IList<Color> Colors { get; set; }
+    public string Category { get; set; }
+    public string Brand { get; set; }
+    public IList<string> Colors { get; set; }
 }
