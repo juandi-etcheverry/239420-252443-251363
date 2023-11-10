9813f07f6b3db2f74c0629924f2ab09dc9426897
Mateo <mateo.goldwasser@hotmail.com>
2023-11-10 15:41:48 -0300

    [GREEN] BrandRepositoryTest

---
+++ DataAccess/BrandRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class BrandRepository: IBrandRepository
+    {
+        private readonly DbContext _context;
+
+        public BrandRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Brand> GetAllBrands()
+        {
+            return _context.Set<Brand>().ToList();
+        }
+    }
+}
