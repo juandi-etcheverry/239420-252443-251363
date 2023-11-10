842d42a96fc53ca637595bf6c5af69712a35b8ba
Mateo <mateo.goldwasser@hotmail.com>
2023-11-10 15:16:50 -0300

    [GREEN] GetCategories_OK

---
+++ DataAccess/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class CategoryRepository: ICategoryRepository
+    {
+        private readonly DbContext _context;
+
+        public CategoryRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> GetAllCategories()
+        {
+            return _context.Set<Category>().ToList();
+        }
+    }
+}
